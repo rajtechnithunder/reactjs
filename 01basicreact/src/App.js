92fc285ac1066d5df9dbd5d4ba5a1bfdfc4ec7af
rajtechnithunder <rchavda@technithunder.com>
2024-05-31 16:39:05 +0530

    create code file

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,10 +1,12 @@
 import Chai from "./Chai";
+import Code from "./Code";
 
 function App() {
   return (
     <div className="App">
       <h1>React JS</h1>
       <Chai />
+      <Code />
     </div>
   );
 }
