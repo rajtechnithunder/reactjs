d8aa76e5279940d3964ca82e8a8024cb21112e5c
rajtechnithunder <rchavda@technithunder.com>
2024-06-18 12:05:07 +0530

    did debugging and solve some essue

--- 11MegaBlog/src/pages/Post.jsx
+++ 11MegaBlog/src/pages/Post.jsx
@@ -12,7 +12,7 @@ function Post() {
 
   const userData = useSelector((state) => state.auth.userData)
 
-  const isAuthor = post && userData ? post.userId === userData.$Id : false
+  const isAuthor = post && userData ? post.userId === userData.$id : false
 
   useEffect(() => {
     if (slug) {
